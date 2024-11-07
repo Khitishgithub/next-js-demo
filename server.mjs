9ad79e178d22c4a8359e5b64af53628ddf765108
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2024-11-07 21:57:19 +0530

    chnages done

---
+++ server.mjs
@@ -0,0 +1,21 @@
+import express from 'express';
+import next from 'next';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Serve Next.js pages
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  const PORT =  3000;
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${PORT}`);
+  });
+});
